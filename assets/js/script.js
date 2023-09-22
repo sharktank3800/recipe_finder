7d1cd0834f5ce11d29a3730db2b84e6d1cd77603
jarrodkrauszer <96881754+jarrodkrauszer@users.noreply.github.com>
2023-09-22 16:55:56 -0400

    worked on cleaning up the code and navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 var heroEl = document.querySelector('.hero-body');
-var mealsEl = document.querySelector('#meals');
+var homePageEl = document.querySelector('#home-page');
+var mealsPageEl = document.querySelector('#meals-page');
+var recipePageEl = document.querySelector('#recipe-page');
+var categorySearch = document.querySelector('#categories');
+var searchButton = document.querySelector('#search-button');
+var searchInput = document.querySelector('#search-input');
 
 // var header = document.querySelector('.')
+const baseURL = 'https://www.themealdb.com/api/json/v1/1/'
 const headers = {
   "X-Api-Key": 'gF3CYb8qvTBIBHse8onGdQ==BKVsjSYgIPJqB3iw'
+}
+
+function displayMealPage() {
+  mealsPageEl.classList.remove('hide');
+  homePageEl.classList.add('hide');
+  recipePageEl.classList.add('hide')
+}
+
+function displayRecipePage() {
+  recipePageEl.classList.remove('hide');
+  homePageEl.classList.add('hide');
+  mealsPageEl.classList.add('hide')
 }
 
 function getQuote() {
@@ -26,44 +44,153 @@ function getQuote() {
 
 }
 
-function fetchMeals() {
-  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=chicken`)
-    .then(function (response) {
-      return response.json();
-    }).then(function(data) {
-      console.log(data);
-      mealsEl.innerHTML = '';
-      data.meals.forEach(function(meal) {
+function displayRecipe(data) {
+  console.log(data);
+  displayRecipePage();
+
+  recipePageEl.innerHTML = '';
+      
+  var ingredientsArray = [];
+  var recipeSlot = data.meals[0];
+
+  var h1 = document.createElement('h1');
+  var div = document.createElement('div');
+  var aside = document.createElement('aside');
+  var section = document.createElement('section');
+  var imgTag = document.createElement('img');
+  var pDiv = document.createElement('div');
+
+  var header = document.createTextNode(data.meals[0].strMeal);
+  var instruction = document.createTextNode(data.meals[0].strInstructions);
+
+  imgTag.src = data.meals[0].strMealThumb + '/preview';
+  imgTag.alt = "meal photo";
+  aside.append(imgTag);
+
+  for(let i = 1; i <= 20; i++) {
+    var p = document.createElement('p');
+    var ingredientsKey = `strIngredient${i}`;
+    var measurementKey = `strMeasure${i}`;
+    var ingredientValue = recipeSlot[ingredientsKey];
+    var measurementValue = recipeSlot[measurementKey]
         
-        var div = document.createElement('div');
-        var imgTag = document.createElement('img');
-        var h4 = document.createElement('h4');
+    if(!ingredientValue == "" || !measurementValue == '') {
+      var ingredients = document.createTextNode(`${measurementValue}    ${ingredientValue}`);
+          
+      p.append(ingredients);
+      pDiv.append(p);
+      aside.append(pDiv);
+    } else {
+      break;
+    }
 
-        imgTag.src = meal.strMealThumb + '/preview';
-        imgTag.alt = 'Meal Image';
+  }
 
-        var mealName = document.createTextNode(meal.strMeal);
-        h4.innerText = meal.strMeal;
+  h1.append(header);
+  section.append(instruction);
+      
+  h1.className = 'is-size-1 my-5';
+  aside.className = 'is-flex is-flex-direction-column is-align-items-center flex-1 ml-6 py-4 mt-5';
+  section.className = 'flex-2 is-size-4 ';
+  div.className = 'is-flex is-flex-direction-row is-align-items-center mx-6';
+  pDiv.className = 'is-flex is-align-items-flex-start is-flex-direction-column is-size-5';
+  imgTag.className = 'mb-6';
 
-        div.className = 'is-flex is-flex-direction-column is-align-items-center mx-3'
-        imgTag.className = 'image is-128x128'
-        h4.className = 'is-size-8'
+  div.append(aside, section);
+  recipePageEl.append(h1, div);
 
-        // div.append(imgTag, mealName);
-        // Adjusting to h4 because textNode cannot be styled
-        div.append(imgTag, h4);
+  meal = {
+    name: data.meals[0].strMeal,
+      id:  data.meals[0].idMeal
+  }
 
-        mealsEl.append(div);
-    });
+  addItemToStorage(meal);
+}
+
+function displayMeals(data) {
+  displayMealPage();
+  console.log(data);
+
+  mealsPageEl.innerHTML = '';
+  data.meals.forEach(function(meal) {
+        
+    var div = document.createElement('div');
+    var imgTag = document.createElement('img');
+    var h4 = document.createElement('h4');
+
+    imgTag.src = meal.strMealThumb + '/preview';
+    imgTag.alt = 'Meal Image';
+
+    imgTag.dataset.filter = 'lookup.php?i=';
+    imgTag.dataset.search = meal.idMeal;
+
+    h4.dataset.filter = 'lookup.php?i=';
+    h4.dataset.search = meal.idMeal;
+
+    // var mealName = document.createTextNode(meal.strMeal.slice(0, 25));
+    h4.innerText = meal.strMeal.slice(0, 25);
+
+    div.className = 'is-flex is-flex-direction-column is-align-items-center mx-3';
+    imgTag.className = 'image is-128x128';
+    h4.className = 'is-size-8';
+
+    // div.append(imgTag, mealName);
+    // Adjusting to h4 because textNode cannot be styled
+    div.append(imgTag, h4);
+
+    mealsPageEl.append(div);
   });
 }
 
-window.addEventListener('load', fetchMeals);
-console.log('Hello');
+function fetchData(filter, search) {
+  console.log(`${baseURL}${filter}${search}`);
+  fetch(`${baseURL}${filter}${search}`)
+    .then(function (response) {
+      return response.json();
+    }).then(function(data) {
+      console.log('Fetch: ' + data);
+
+      if(filter === 'filter.php?c=' || filter === 'search.php?s=') {
+        displayMeals(data);
+      } else if (filter === 'lookup.php?i=') {
+        displayRecipe(data);
+      }
+      
+  });
+}
+
+function searchMealDB(e) {
+  console.log(e.target.dataset.filter, e.target.dataset.search);
+
+  if(e.target.dataset.filter && e.target.dataset.search){
+    fetchData(e.target.dataset.filter, e.target.dataset.search.toString())
+  } else if(e.target.dataset.filter) {
+    fetchData(e.target.dataset.filter, searchInput.value);
+  }
+  
+}
+
+function addItemToStorage(item) {
+  const itemsFromStorage = getItemsFromStorage();
+
+  if(!itemsFromStorage.includes(item)) {
+    itemsFromStorage.push(item);
+
+    localStorage.setItem('meals', JSON.stringify(itemsFromStorage));
+  }
+  
+}
+
+function getItemsFromStorage() {
+  return JSON.parse(localStorage.getItem('meals')) || [];
+}
+
+categorySearch.addEventListener('click', searchMealDB);
+searchButton.addEventListener('click', searchMealDB);
+mealsPageEl.addEventListener('click', searchMealDB);
+
+
+getQuote();
 setInterval(getQuote, 30000);
 
-// function hidePage() {
-   // header.classlist.add("hide")
-  //  section.classlist.add)("hide")
-  
 
